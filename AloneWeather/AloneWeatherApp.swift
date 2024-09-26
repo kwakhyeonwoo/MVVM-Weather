9041b0b3710d6de1e7c20302a1fb33b75d70d75c
곽현우 <polla723@naver.com>
2024-09-26 16:49:15 +0900

    Initial Commit

---
+++ AloneWeather/AloneWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  AloneWeatherApp.swift
+//  AloneWeather
+//
+//  Created by 곽현우 on 9/26/24.
+//
+
+import SwiftUI
+
+@main
+struct AloneWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
